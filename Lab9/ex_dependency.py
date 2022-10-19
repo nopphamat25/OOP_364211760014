996efc3650c8bd9e39947ca67062d22fdc5eebc5
yanisa <yanisa.o@rmutsvmail.com>
2022-10-19 17:53:22 +0700

    add: Lab9

---
+++ Lab9/ex_dependency.py
@@ -0,0 +1,36 @@
+"""
+name: {Nopphamat Charornlap}
+ID:{364211760014}
+Group:{MIT221}
+"""
+
+#Class Relation - Dependency
+
+from datetime import date
+
+class Customer:
+    def __init__(self,cusid,name):
+        self.cusid = cusid
+        self.name = name
+
+    def customer_info(self):
+        return f'CusID:{self.cusid} Customer name:{self.name}'
+
+class Order:
+    def __init__(self,orderid,date,Customer,total):
+        self.orderid = orderid
+        self.date = date
+        self.customer = Customer
+        self.total = total
+    def order_info(self):
+        print("Order description:")
+        print(f'\tCustomer info: {self.customer.customer_info()}')
+        print(f'\tDate:{self.date}')
+        print(f'\tTotal cost: {self.total}')
+
+#create object
+cus1 = Customer("CUS001","Nopphamat Charoenlap")
+
+order1 = Order("ORDER001",date.today(),cus1,15000)
+
+order1.order_info()
